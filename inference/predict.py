24532022255f8d3c0075c1bd3d788fb36a661373
SHAURYA SINGHAL <shaurya.singhal_cs.aiml24@gla.ac.in>
2026-03-05 15:59:43 +0530

    Organized testing dataset into fire and nofire folders

--- inference/predict.py
+++ inference/predict.py
@@ -1,38 +1,36 @@
 import torch
 import torch.nn.functional as F
-from torchvision import transforms
 from PIL import Image
-from model import get_model
 
-# Device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from models.model import get_model
+from preprocessing.transforms import transform
 
-# Load trained model
-model = get_model(2)
-model.load_state_dict(torch.load("fire_model.pth", map_location=device))
-model.to(device)
+# load model
+model = get_model(num_classes=2)
+model.load_state_dict(torch.load("saved_models/fire_model.pth"))
 model.eval()
 
-# Image transform (must match training)
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-])
 
-# 🔥 PUT YOUR IMAGE PATH HERE
-image_path = "/Users/shauryasinghal/Desktop/flame2-ml/dataset/Forest Fire Dataset/Training/fire/fire_0008.jpg"
+def predict_image(image_path):
 
-# Load image
-image = Image.open(image_path).convert("RGB")
-image = transform(image).unsqueeze(0).to(device)
+    # open image
+    image = Image.open(image_path).convert("RGB")
 
-# Prediction
-with torch.no_grad():
-    output = model(image)
-    probabilities = F.softmax(output, dim=1)
-    confidence, pred = torch.max(probabilities, 1)
+    # apply preprocessing
+    image = transform(image)
 
-classes = ["fire", "nofire"]
+    # add batch dimension
+    image = image.unsqueeze(0)
 
-print("\nPrediction:", classes[pred.item()])
-print("Confidence: {:.2f}%".format(confidence.item() * 100))
+    # prediction
+    with torch.no_grad():
+
+        outputs = model(image)
+
+        probabilities = F.softmax(outputs, dim=1)
+
+        confidence, predicted = torch.max(probabilities, 1)
+
+    classes = ["fire", "nofire"]
+
+    return classes[predicted.item()], confidence.item()
